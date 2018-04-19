1859cd89f8aba2f1d9afad0ada2cd8db3354e8dd
tatisouto <tatisouto@gmail.com>
2018-04-19 14:11:05 -0300

    commit alter route

--- Projeto.Agenda.MVC/App_Start/RouteConfig.cs
+++ Projeto.Agenda.MVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Projeto.Agenda.MVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Agenda", action = "List", id = UrlParameter.Optional }
             );
         }
     }
